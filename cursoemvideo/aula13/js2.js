b75d598e61e273ef467dfa2e62a7e6fe091e1b54
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-06 16:19:13 -0300

    done

--- cursoemvideo/aula13/js2.js
+++ cursoemvideo/aula13/js2.js
@@ -4,8 +4,20 @@ function clicou() {
     var anoAtual = ano.getFullYear()
     var idade = anoAtual - Number(anocliente.value)
     var texto = document.getElementById('calculo')
+    var imagem = document.getElementById('imagem')
 
-    
+    var sexIdentificadorMasc = document.getElementById('masc')
+    var sexIdentificadorFem = document.getElementById('fem')
 
-    texto.innerHTML = `Você está atualmente com ${idade} anos `
+    if (sexIdentificadorMasc.checked == true) {
+        texto.innerHTML = `Detectamos um Homem com ${idade} anos `
+
+        if(idade > 0 && idade <= 12) {
+            imagem.src = 'imagens/icon1.png'
+        } else if(idade > 13 && idade <= 35) {
+            imagem.src = 'imagens/icon2.png'
+        }
+    } else {
+        texto.innerHTML = `Detectamos uma Mulher com ${idade} anos `
+    }
 }
