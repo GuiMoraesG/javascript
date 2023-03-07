fbd9a406d3b86f045e1a270088444366746915fc
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-07 15:52:32 -0300

    aula15 ex002

---
+++ cursoemvideo/aula15/ex002.js
@@ -0,0 +1,21 @@
+function contar() {
+    var num = document.getElementById('num1')
+    var N = Number(num.value)
+    var Tab = document.getElementById('tab')
+
+
+
+    if (num.value.length == 0) {
+        alert(`Por favor, digite um número válido ${N}`)
+    } else {
+        for (var c = 0; c <= 10; c++) {
+            var item = document.createElement('option')
+            item.text += `${N} X ${c} = ${N * c} `
+
+            Tab.appendChild(item)
+        }
+    }
+
+
+
+}
