0333afeb97a5043734fa83c727ea3d32e865e406
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-08 11:29:11 -0300

    aula15

---
+++ cursoemvideo/aula16/ex002.js
@@ -0,0 +1,7 @@
+let num = [8, 5, 7, 9, 9]
+
+num.sort()
+
+for(let pos in num) {
+    console.log(num[pos])
+}
---
+++ cursoemvideo/aula16/ex01.js
@@ -0,0 +1,14 @@
+let num = [5, 8, 2, 9, 3]
+// num.push(3)
+
+console.log(`ssssssssss ${num.indexOf(8)}  `)
+
+// console.log(num)
+console.log(`O Vetor tem ${num.length} posições`)
+// console.log(`O primeiro valor do nosso array é ${num[0]} `)
+
+
+
+for(var pos = 0; pos < num.length; pos++) {
+    console.log(num[pos])
+}
