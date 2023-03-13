a40096af8be32c60ca4f1e1117eaa253c4aa7363
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-13 15:52:07 -0300

    aula18

--- cursoemvideo/aula18/js.js
+++ cursoemvideo/aula18/js.js
@@ -1,5 +1,4 @@
 var txtN = document.getElementById('num')
-var txtNV = Number(txtN.value)
 var valores = []
 var res = document.getElementById('res')
 var texto = document.createElement('option')
@@ -8,10 +7,16 @@ var box = document.getElementById('box')
 function verificar() {
     if (isNumero(txtN) && !inLista(txtN, valores)) {
         valores.push(Number(txtN.value))
-
+        var item = document.createElement('option')
+        item.text += `O valor ${txtN.value} foi adicionado `
+        box.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('Por favor, digite um número válido !!!')
     }
+
+    txtN.value = ''
+    txtN.focus()
 }
 
 function isNumero(N1) {
@@ -27,5 +32,35 @@ function inLista(n, l) {
         return true
     } else {
         return false
+    }
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Por favor, adicionar valores !!!')
+    } else {
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+
+        for (var pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            } else if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>A quantidade de valores digitados foram ${tot} </p>`
+        res.innerHTML += `<p>O menor valor digitado foi ${maior} </p>`
+        res.innerHTML += `<p>O menor valor digitado foi ${menor} </p>`
+        res.innerHTML += `<p>A soma dos valores digitado é ${soma} </p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media} </p>`
+
     }
 }
