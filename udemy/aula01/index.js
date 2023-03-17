26f390da12387541bf6cb39d2975a0ae9cd8e9db
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-17 12:58:27 -0300

    aula01

---
+++ udemy/aula01/index.js
@@ -0,0 +1,7 @@
+var data = new Date()
+var hora = data.getHours()
+var min = data.getMinutes()
+var seconds = data.getSeconds()
+
+console.log(`Meu Nome é "GUILHERME DE MORAES GARCIA". e estou aprendendo JavaScript ás ${hora}:${min}:${seconds} Horas`)
+console.log(`Meu Nome é "GUILHERME DE MORAES GARCIA". e estou aprendendo JavaScript ás`, 10, ` Horas`)
