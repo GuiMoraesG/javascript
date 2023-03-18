98bce2104fd12907288b5800c9a1e0631c2ca0a4
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-18 19:45:37 -0300

    aula07

---
+++ udemy/aula07/ex001.js
@@ -0,0 +1,7 @@
+let superString = 'Um texto legal'
+
+console.log(superString.indexOf(' '))
+console.log(superString.replace('Um', 'Obrigado'))
+console.log(superString.length)
+console.log(superString.slice(3, 8))
+console.log(superString.split(' '))
