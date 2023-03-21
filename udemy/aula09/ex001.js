fbb89fce119fc2eac53dab6b981039787554321e
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-21 16:57:34 -0300

    aula09

---
+++ udemy/aula09/ex001.js
@@ -0,0 +1,8 @@
+let num1 = 5.633
+let resultado = Math.round(num1)
+let aleatorio = Math.round(Math.random() * (10 - 0) + 0)
+
+console.log(resultado)
+console.log(Number.isInteger(resultado))
+console.log(Math.max(4, 3, 2, 9))
+console.log(aleatorio)
