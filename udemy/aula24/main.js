486b47817589ebd6473e6c3b988682e8f3b9687d
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-28 19:32:53 -0300

    aula24

---
+++ udemy/aula24/main.js
@@ -0,0 +1,17 @@
+const elementos = [
+    { tag: 'p', texto: 'Frase 1' },
+    { tag: 'div', texto: 'Frase 2' },
+    { tag: 'section', texto: 'Frase 3' },
+    { tag: 'footer', texto: 'Frase 4' }
+]
+const res = document.getElementById('res')
+const div = document.createElement('div')
+
+for (let i = 0; i < elementos.length; i++) {
+    let {tag, texto} = elementos[i]
+    let tagCriada = document.createElement(tag)
+    tagCriada.innerHTML = texto
+    div.appendChild(tagCriada)
+}
+
+res.appendChild(div)
