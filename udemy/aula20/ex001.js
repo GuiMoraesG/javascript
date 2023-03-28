8a96ee5d88b00af33c4bb83ea5d5efa1f5082aa7
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-28 15:53:52 -0300

    aula20

---
+++ udemy/aula20/ex001.js
@@ -0,0 +1,18 @@
+// let a = 'A';
+// let b = 'B';
+// let c = 'C';
+// const numeros = [1, 2, 3];
+
+// [a, b, c] = numeros
+
+// console.log(a, b, c)
+
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const [primeiroNumero, segundoNumero, , ,quintoNumero, ...resto] = numeros
+
+// console.log(primeiroNumero, segundoNumero, quintoNumero, resto)
+
+const numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+const [lista1, lista2, lista3] = numeros
+
+console.log(lista2[0])
