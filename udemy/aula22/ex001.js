88ad8d225e338370177e8516621ca9c80f8ca77a
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-28 16:46:07 -0300

    aula22

---
+++ udemy/aula22/ex001.js
@@ -0,0 +1,17 @@
+// for (let c = 0; c <= 10; c++) {
+//     if (c % 2 == 0) {
+//         console.log(c, 'Par')
+//     } else {
+//         console.log(c, 'IMPAR')
+//     }
+// }
+
+const frutas = ['MAÇA', 'BANANA', 'MELANCIA']
+
+for (let c = 0; c < frutas.length; c++) {
+    console.log(frutas[c])
+}
+
+for (let c in frutas) {
+    console.log(frutas[c])
+}
