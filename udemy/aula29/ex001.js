fcc473be1e96598223204e52cff4f590442490e2
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-31 13:45:22 -0300

    aula 29 ex001

---
+++ udemy/aula29/ex001.js
@@ -0,0 +1,12 @@
+// função que recebe 2 numeros e retorne o maior.
+
+
+function maiorMenor(x, y) {
+    if (x > y) {
+        return x 
+    } else {
+        return y
+    }
+}
+
+console.log(maiorMenor(15, 10))
