1bbd126702d842da4cde26a7df8cca00342f49ac
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-03-31 13:52:01 -0300

    att

--- udemy/aula29/ex001.js
+++ udemy/aula29/ex001.js
@@ -1,12 +1,13 @@
 // função que recebe 2 numeros e retorne o maior.
 
+const maiorMenor2 = (x, y) => x > y ? x : y;
 
 function maiorMenor(x, y) {
     if (x > y) {
-        return x 
+        return x
     } else {
         return y
     }
 }
 
-console.log(maiorMenor(15, 10))
+console.log(maiorMenor2(15, 10))
