9d077193b923c0f552a4b0c8b451faa18bd7461b
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-04-14 12:06:24 -0300

    aula44 finalizada

--- udemy/aula44/ex002.js
+++ udemy/aula44/ex002.js
@@ -3,10 +3,12 @@ const pessoas = [
     { nome: 'Pedro', idade: 30 },
     { nome: 'Lais', idade: 23 },
     { nome: 'Roberto', idade: 18 },
-    { nome: 'Ryu', idade: 10 },
+    { nome: 'Ryua', idade: 10 },
     { nome: 'Abigail', idade: 88 },
 ]
 
 const pessoasNome = pessoas.filter(obj => obj.nome.length >= 7)
+const idadeAvancada = pessoas.filter(valor => valor.idade >= 50)
+const terminaComA = pessoas.filter(valor => valor.nome.toLowerCase().endsWith('a'))
 
-console.log(pessoasNome)
+console.log(terminaComA)
