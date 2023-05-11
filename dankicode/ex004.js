0e43ce8ce4641b666ddb8b2d8a03bf423d664d68
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-11 17:17:18 -0300

    aula004

---
+++ dankicode/ex004.js
@@ -0,0 +1,12 @@
+let boleano = true
+let num = 4
+let caracteres = 'Guilherme'
+let array = []
+let objeto = {}
+
+
+console.log(typeof boleano)
+console.log(typeof num)
+console.log(typeof caracteres)
+console.log(typeof array)
+console.log(typeof objeto)
