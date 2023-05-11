c6645cf0b2ada831f92b2ade2c2d69737821b30f
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-11 18:01:04 -0300

    aula005

---
+++ dankicode/ex005.js
@@ -0,0 +1,8 @@
+const nome = 'GUILHERME'
+const nomeReformulado = nome.toLowerCase()
+
+if(nomeReformulado == 'guilherme') {
+    console.log(`Olá, ${nomeReformulado}`)
+} else {
+    console.log(`O nome ${nomeReformulado} não é conpativel`)
+}
