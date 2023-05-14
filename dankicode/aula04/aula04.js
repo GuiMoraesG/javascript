1086150d01094232aef45278e956a0877c24a1aa
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-13 23:09:20 -0300

    alterações

--- dankicode/aula04/aula04.js
+++ dankicode/aula04/aula04.js
@@ -1,7 +1,6 @@
 class Produtos {
     constructor() {
         this.carrinho = document.querySelector('.carrinho')
-        this.li = document.createElement('li')
         this.quantidadeItem1 = 0
         this.quantidadeItem2 = 0
         this.quantidadeItem3 = 0
@@ -14,18 +13,21 @@ class Produtos {
 
             if (el.classList.contains('tic-tac')) {
                 e.preventDefault()
+
                 this.quantidadeItem1++
                 this.ticTac()
             }
 
             if (el.classList.contains('skol')) {
                 e.preventDefault()
+
                 this.quantidadeItem2++
                 this.skol()
             }
 
             if (el.classList.contains('ruffles')) {
                 e.preventDefault()
+
                 this.quantidadeItem3++
                 this.ruffles()
             }
@@ -33,19 +35,19 @@ class Produtos {
     }
 
     ticTac() {
-        const texto = this.li
+        const texto = document.createElement('div')
         texto.innerHTML = `Tic-Tac | Quantidade: ${this.quantidadeItem1}`
         this.carrinho.appendChild(texto)
     }
 
     skol() {
-        const texto = this.li
+        const texto = document.createElement('div')
         texto.innerHTML = `SKOL | Quantidade: ${this.quantidadeItem2}`
         this.carrinho.appendChild(texto)
     }
 
     ruffles() {
-        const texto = this.li
+        const texto = document.createElement('div')
         texto.innerHTML = `Ruffles | Quantidade: ${this.quantidadeItem3}`
         this.carrinho.appendChild(texto)
     }
