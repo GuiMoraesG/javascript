80a22675abff67bb456779b7c116ce9a1bda0a99
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-16 18:44:19 -0300

    aula009

--- dankicode/aula09-jquery/function.js
+++ dankicode/aula09-jquery/function.js
@@ -11,4 +11,12 @@ $(function () {
             location.href = 'http://127.0.0.1:5500/aula09-jquery/'
         }, 1000)
     })
+
+    $('.box2').click(e => {
+        e.stopPropagation()
+    })
+
+    $('body').click(() => {
+        $('.box2').css('background', 'green')
+    })
 })
