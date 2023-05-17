369722b70c0aba400ddbc7e8992853ad8fbaa81e
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-17 18:01:41 -0300

    aula14

---
+++ dankicode/aula14-jquery/function.js
@@ -0,0 +1,21 @@
+$('.block').on({
+    click: () => {
+        $('.block').css('background', 'blue')
+        $('#texto').text('Clicou no quadrado!!')
+    },
+
+    mouseout: () => {
+        $('#texto').text('Fora do quadrado!!')
+        $('.block').css('background', 'red')
+    },
+
+    mouseenter: () => {
+        $('#texto').text('Entrou no quadrado!!')
+        $('.block').css('background', 'black')
+    }
+})
+
+
+$('.block2').click(() => {
+    $('.block').trigger('click')
+})
