70f217b096a03ee4cc5f5654dbaba935eec3fded
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-18 16:25:59 -0300

    aula18

---
+++ dankicode/aula18-jquery/function.js
@@ -0,0 +1,7 @@
+$('p').parents().css({ 'border': '1px solid green' })
+$('p').parentsUntil('div').css({ 'border': '1px solid red' })
+
+// $('li').parent().css({'border': '1px solid red'})
+// $('ul').parent().css({'border': '1px solid green'})
+// $('div').parent().css({'border': '1px solid purple'})
+// $('body').parent().css({'border': '1px solid yellow'})
