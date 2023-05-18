64d97c788bb480171fba1e1706b39265c9a7ddc1
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-18 16:54:28 -0300

    aula20

---
+++ dankicode/aula20-jquery/function.js
@@ -0,0 +1,9 @@
+// $('h3').siblings().css({ 'border': '1px solid red' })
+
+// $('h3').siblings('p').css({ 'border': '1px solid red' })
+
+// $('h3').next().css({ 'border': '1px solid red' })
+
+$('h2').prev().css({ 'border': '1px solid red' })
+
+
