ba1d1ceceb36a0acc5ba13c16bca08472a332ef0
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-22 13:19:21 -0300

    aula27

---
+++ dankicode/aula27-jquery/function.js
@@ -0,0 +1,9 @@
+$('#btn-aumentar').click(function () {
+    $('#caixa').animate({ opacity: 'toggle' })
+})
+
+$('#btn-diminuir').click(function () {
+    $('#caixa').animate({ width: 'toggle', height: 'toggle' })
+})
+
+
