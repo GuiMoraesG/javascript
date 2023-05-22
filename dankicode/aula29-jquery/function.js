700a3c9b6ae5f18a30fb9bc22c2e7163afe43dad
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-22 13:50:19 -0300

    aula29

---
+++ dankicode/aula29-jquery/function.js
@@ -0,0 +1,11 @@
+$('#btn-mostrar').click(function () {
+    $('#caixa').fadeIn()
+})
+
+$('#btn-ocultar').click(function () {
+    $('#caixa').fadeOut()
+})
+
+$('#btn-mostraOcultar').click(function () {
+    $('#caixa').fadeOut().fadeIn()
+})
