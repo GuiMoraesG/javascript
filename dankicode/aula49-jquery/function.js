022522309bc326ad6b7df244f3eee597893948c4
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-25 17:33:41 -0300

    aula49

---
+++ dankicode/aula49-jquery/function.js
@@ -0,0 +1,30 @@
+$('#btnRemoverAtributo').click(() => {
+    $('#caixa').removeAttr('style')
+})
+
+$('#btnRemoverClass').click(() => {
+    $('#caixa').removeClass('borda')
+})
+
+
+
+$('#btnPosition').click(() => {
+    let pt = $('#caixa').position().top
+    let pl = $('#caixa').position().left
+    let ot = $('#caixa').offset().top
+    let ol = $('#caixa').offset().left
+
+    alert(`TOP P: ${pt} | LEFT P: ${pl} | TOP O: ${ot} | LEFT O: ${ol}`)
+})
+
+$('#top').change(() => {
+    let valor = document.getElementById('top').value
+
+    $('#caixa').offset({ 'top': valor })
+})
+
+$('#left').change(() => {
+    let valor = document.getElementById('left').value
+
+    $('#caixa').offset({ 'left': valor })
+})
