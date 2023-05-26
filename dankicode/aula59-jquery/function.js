496300b601dcce25f78865da374116df36ed5396
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-26 01:04:42 -0300

    aula59

---
+++ dankicode/aula59-jquery/function.js
@@ -0,0 +1,5 @@
+let cx = $('#caixa1')
+
+$('#info').html(
+    'width: ' + cx.width() + '<br>  Height: ' + cx.height() + '<br> innerWidth: ' + cx.innerWidth() + '<br> innerHeight: ' + cx.innerHeight() + '<br> outerWidth: ' + cx.outerWidth() + '<br> outerHeight: ' + cx.outerHeight() + '<br> outerWidth(true): ' + cx.outerWidth(true) + '<br> outerHeight(true): ' + cx.outerHeight(true)
+)   
