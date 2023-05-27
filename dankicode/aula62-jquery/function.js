435cb74175976d30c80304dbf6b26800e68f13f0
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-05-27 16:44:20 -0300

    att

--- dankicode/aula62-jquery/function.js
+++ dankicode/aula62-jquery/function.js
@@ -2,12 +2,17 @@ $('.box').click(function () {
     $('.box').width(600)
     $('.box').height(600)
 
+    $('.test').text('O QUADRADO ABAIXO FOI CLICADO !!!')
+    $('.box').append('<p>Elemento criado pelo click do mouse</p>')
+
     atualiza()
 })
 
 $('.box').mouseout(function () {
     $('.box').width(500)
     $('.box').height(500)
+
+    $('.test').text('O MOUSE SAIU DO QUADRADO ABAIXO !!!')
 
     atualiza()
 })
