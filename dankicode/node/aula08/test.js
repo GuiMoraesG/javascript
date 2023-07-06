a14a9726fee3eeda341b00370d02d8516b5298d2
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-07-06 18:55:27 -0300

    aula08

--- dankicode/node/aula08/test.js
+++ dankicode/node/aula08/test.js
@@ -3,3 +3,27 @@ const sequelize = new Sequelize('testes', 'root', 'Gui010300!', {
     host: 'localhost',
     dialect: 'mysql'
 })
+
+const Postagem = sequelize.define('postagens', {
+    titulo: { type: Sequelize.STRING },
+    conteudo: { type: Sequelize.TEXT }
+})
+
+Postagem.create({
+    titulo: "titulo de teste",
+    conteudo: "loremmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"
+})
+
+const Usuario = sequelize.define('usuarios', {
+    nome: { type: Sequelize.STRING },
+    sobrenome: { type: Sequelize.STRING },
+    idade: { type: Sequelize.INTEGER },
+    email: { type: Sequelize.STRING }
+})
+
+Usuario.create({
+    nome: 'Guilherme',
+    sobrenome: 'Moraes',
+    idade: 23,
+    email: 'gui@gmail.com'
+})
