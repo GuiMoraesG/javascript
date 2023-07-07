edfe7a5ee9112b0e41c1468c7d8fe0fd84381dd5
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-07-07 15:19:57 -0300

    aula10 - att

--- dankicode/node/aula10/index.js
+++ dankicode/node/aula10/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize')
 let handle = handlebars.create({ defaultLayout: 'main' })
 
 
 app.engine('handlebars', handle.engine)
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const sequelize = new Sequelize('testes', 'root', 'Gui010300!', {
     host: 'localhost',
@@ -18,7 +22,7 @@ app.get('/cadastro', (req, res) => {
 })
 
 app.post('/add', (req, res) => {
-    res.send('Formulario recebido')
+    res.send(`Texto: ${req.body.titulo} Conteudo: ${req.body.conteudo}`)
 })
 
 app.listen(8081, () => {
