42133ad3648c1fcfad5840f53f6a676341bcc197
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-07-11 15:48:39 -0300

    aula18

--- udemy/node/aula18-agenda/src/controllers/loginController.js
+++ udemy/node/aula18-agenda/src/controllers/loginController.js
@@ -1,3 +1,33 @@
+// const { async } = require('regenerator-runtime')
+const Login = require('../models/LoginModel')
+
 exports.index = (req, res) => {
     res.render('login')
+}
+
+exports.registro = async (req, res) => {
+    try {
+        const login = new Login(req.body)
+        await login.registro()
+
+        if (login.error.length > 0) {
+            req.flash('erros', login.error)
+            req.session.save(() => {
+                return res.redirect('back')
+            })
+            return
+        }
+
+        req.flash('sucesso', 'Seu Usuário foi criado com sucesso')
+        req.session.save(() => {
+            return res.redirect('back')
+        })
+
+    } catch (e) {
+        console.log(e)
+        return res.render('404')
+    }
+
+
+
 }
