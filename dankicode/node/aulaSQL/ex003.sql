70720ace1204a3e22105be5cf9fc5dc708855db6
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-07-19 16:02:20 -0300

    aula SQL-03

---
+++ dankicode/node/aulaSQL/ex003.sql
@@ -0,0 +1,19 @@
+INSERT INTO pessoas values(
+    default,
+    "Dante",
+    "2000-12-30",
+    "M",
+    "80.80",
+    "1.90",
+    default
+),
+(   default,
+    "Vergil",
+    "2000-12-30",
+    "M",
+    "80.80",
+    "1.90",
+    default
+);
+
+SELECT * FROM pessoas; 
