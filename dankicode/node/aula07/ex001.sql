a8074e470bf8990948cb4207b9a2156562dcdaf4
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-07-19 14:03:38 -0300

    Estudos SQL-01

--- dankicode/node/aula07/ex001.sql
+++ dankicode/node/aula07/ex001.sql
@@ -2,4 +2,4 @@ insert into usuarios(nome, email, idade) values(
     "davo",
     "ricardo@gmail.com",
     55
-); 
+);
---
+++ dankicode/node/aulaSQL/ex001.sql
@@ -0,0 +1,8 @@
+create table pessoas(
+    nome VARCHAR(30),
+    idade TINYINT(3),
+    sexo CHAR(1),
+    peso FLOAT,
+    altura FLOAT,
+    nacionalidade VARCHAR(20)
+);
