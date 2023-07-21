0ecd27b72cb18007242d1a698e0cc1903642c456
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-07-21 18:37:43 -0300

    aula SQL-09

---
+++ dankicode/node/aulaSQL/ex009.sql
@@ -0,0 +1,9 @@
+alter table alunos add foreign key (cursospreferidos) reference cursos (idcursos);
+
+update alunos set cursopreferido = '8' where id = '1';
+
+select alunos.nome, cursos.nome, cursos.ano from alunos join cursos on cursos.idcursos = alunos.cursopreferido;
+
+select alunos.nome, cursos.nome, cursos.ano from alunos left outer join cursos on cursos.idcursos = alunos.cursopreferido;
+
+select alunos.nome, cursos.nome, cursos.ano from alunos right outer join cursos on cursos.idcursos = alunos.cursopreferido;
