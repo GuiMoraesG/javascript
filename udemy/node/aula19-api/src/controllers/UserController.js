d0e63560e597c4042cb4d77017724b7f7a5edd47
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-08-14 16:49:29 -0300

    aula 19 node

--- udemy/node/aula19-api/src/controllers/UserController.js
+++ udemy/node/aula19-api/src/controllers/UserController.js
@@ -15,8 +15,6 @@ class UserController {
   async index(req, res) {
     try {
       const users = await User.findAll();
-      console.log('User ID', req.userId);
-      console.log('User E-mail', req.userEmail);
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -34,12 +32,7 @@ class UserController {
 
   async update(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({
-          errors: ['ID não enviado !'],
-        });
-      }
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
       if (!user) {
         return res.status(400).json({
           errors: ['Usuário não encontrado !'],
@@ -56,12 +49,7 @@ class UserController {
 
   async delete(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({
-          errors: ['ID não enviado !'],
-        });
-      }
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
       if (!user) {
         return res.status(400).json({
           errors: ['Usuário não encontrado !'],
--- udemy/node/aula19-api/src/routes/user.js
+++ udemy/node/aula19-api/src/routes/user.js
@@ -4,10 +4,11 @@ import loguinRequired from '../middlewares/loguinRequired';
 
 const router = new Router();
 
-router.post('/', userController.store);
 router.get('/', loguinRequired, userController.index);
 router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+
+router.post('/', userController.store);
+router.put('/', loguinRequired, userController.update);
+router.delete('/', loguinRequired, userController.delete);
 
 export default router;
