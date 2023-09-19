a0fa88cc19436401db8fc1214e6004fe9430d902
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-09-19 16:19:57 -0300

    Otimização

--- dankicode/node/aula14-jwt/src/models/User.js
+++ dankicode/node/aula14-jwt/src/models/User.js
@@ -19,14 +19,7 @@ class User {
     }
 
     async criar() {
-        if (!this.body.name || !this.body.email || !this.body.password || !this.body.confirmpassword) {
-            return this.erros.push('Todos os campos precisam estar preenchidos')
-        }
-
-        if (this.body.password !== this.body.confirmpassword) {
-            return this.erros.push('Os campos senha preciam ser identicos')
-        }
-
+        this.dataValidation();
         await this.emailValidation();
 
         if (this.erros.length > 0) { return }
@@ -44,6 +37,16 @@ class User {
         const user = await UserModel.findOne({ email: this.body.email });
 
         if (user) { return this.erros.push('E-mail já registrado') }
+    }
+
+    async dataValidation() {
+        if (!this.body.name || !this.body.email || !this.body.password || !this.body.confirmpassword) {
+            return this.erros.push('Todos os campos precisam estar preenchidos')
+        }
+
+        if (this.body.password !== this.body.confirmpassword) {
+            return this.erros.push('Os campos senha preciam ser identicos')
+        }
     }
 }
 
